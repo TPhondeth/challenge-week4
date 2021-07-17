a282d7746741bc53e708a1a8cc7294b60ca7af34
Tony Phondeth <phondeth@gmail.com>
2021-07-17 17:35:10 -0400

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,42 @@
+var quizContainerEl = document.querySelector(".quizContainer");
 var highScoreEl = document.querySelector(".highScore");
 var timerEl = document.querySelector(".timer");
 var startBtn = document.querySelector(".startBtn");
 
+// Event Listeners
+startBtn.addEventListener('click', countdown);
+startBtn.addEventListener('click', showQuestions);
 
 // Questions Object with Array of Questions
-questions = [
+codeQuiz = [
     {
-        questionText: "Commonly used data types DO not include:",
-        questionAnswer: { 1: "alerts", 2: "booleans", 3: "numbers", 4: "strings" },
-        answer: "1",
+        question: "Commonly used data types DO not include:",
+        answers: { 1: "alerts", 2: "booleans", 3: "numbers", 4: "strings" },
+        correctAnswer: "1",
     },
     {
-        questionText: "The condition in an if / else statement in enclosed with _____.",
-        questionAnswer: { 1: "quotes", 2: "curly brackets", 3: "parenthesis", 4: "square brackets"},
-        answer: "3",
+        question: "The condition in an if / else statement in enclosed with _____.",
+        answers: { 1: "quotes", 2: "curly brackets", 3: "parenthesis", 4: "square brackets"},
+        correctAnswer: "3",
     },
     {
-        questionText: "Arrays is Javascript can be used to store _____.",
-        questionAnswer: { 1: "numbers and strings", 2: "other arrays", 3: "booleans", 4: "all of the above"},
-        answer: "4",
+        question: "Arrays is Javascript can be used to store _____.",
+        answers: { 1: "numbers and strings", 2: "other arrays", 3: "booleans", 4: "all of the above"},
+        correctAnswer: "4"
     },
     {
-        questionText: "String variables must be enclosed within _____ when being assigned to variables.",
-        questionAnswer: { 1: "commas", 2: "quotes", 3: "curly brackets", 4: "parenthesis"},
-        answer: "2",
+        question: "String variables must be enclosed within _____ when being assigned to variables.",
+        answers: { 1: "commas", 2: "quotes", 3: "curly brackets", 4: "parenthesis"},
+        correctAnswer: "2"
     }
 ];
 
-// Timer that counts down from 75
+
+// Function for 75 second countdown
 function countdown() {
     var timeLeft = 75;
     var timeInterval = setInterval( function() {
-        timeLeft >= 1;
+        timeLeft--;
         timerEl.textContent = `Time: ${timeLeft}`;
 
     if(timeLeft <= 0) {
@@ -40,5 +45,26 @@ function countdown() {
     },1000)
 }
 
-// Event Listeners
-startBtn.addEventListener('click', countdown);
+// Function to Show Questions
+function showQuestions() {
+        var questionsCounter = 0;
+        var question = codeQuiz[questionsCounter];
+        var output = [];
+        var answers = [];
+    
+        for (answer in question.answers) {
+            answers.push(
+                        `<div>
+                        <button name="question${questionsCounter}" onclick='checkAnswer(${answer})'>${answer} ${question.answers[answer]}</button>           
+                        </div>`
+            );
+        }
+
+        output.push(
+                    `<div id="questions"> ${question.question} </div>
+                    <div id="answers"> ${answers.join("")} </div>`
+    );
+            quizContainerEl.innerHTML = output.join("");
+}
+
+// 
